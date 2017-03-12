4222b534e8b4ffc741c2f140d14ca72c10c8b33d
Rahul Kadam <coderk21@gmail.com>
2017-03-12 14:26:12 +0000

    Updated loadData.R to add sorting and borough data to the analysis

--- R Scripts/dataLoad.R
+++ R Scripts/dataLoad.R
@@ -41,6 +41,13 @@ read.zip <- function(files){
 
 #####Clean Data Function#####
 cleanData <- function(dataset){
-    datasetC<<- dataset[!is.na(dataset$EndStation.Id),];
+    datasetC<- dataset[!is.na(dataset$EndStation.Id),];
+    datasetC$Start.Date <- strptime(x=as.character(datasetC$Start.Date), format = "%d/%m/%Y %H:%M");
+    datasetC$End.Date <- strptime(x=as.character(datasetC$End.Date), format = "%d/%m/%Y %H:%M");
+    datasetS <<- datasetC[order(datasetC$Bike.Id,datasetC$Start.Date),]; # sorts the data by BikeId,StartDate to aid in matching Start and End destinations
+    dBorCord <<- read.csv("..Data/Docking station Boroughs and Coords.csv", header=T); # load Borough and Coordinate data to be added to the final dataset
+    
+    
+    
 }
 #####End of Clean Data Function#####
