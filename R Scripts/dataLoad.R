477fdaa0b718b980198285a044378daf37b167d6
Rahul Kadam <coderk21@gmail.com>
2017-03-13 17:48:06 +0000

    Update to dataLoad.R
    This update adds code to strip the Start and End station names to bare
    site names. These sites names are then used to join to the Borough and
    Coordinate data from the last update to create one wholistic dataset
    which contains all of the necessary information like Borough, Latitude
    and Longitude for both Start and End Stations.

--- R Scripts/dataLoad.R
+++ R Scripts/dataLoad.R
@@ -22,32 +22,54 @@ extractData <- function (filelist) {
 }
 #####End of Extract Data Function#####
 
-#####Read from a Zip File#####
-# This is a sister function that is called in the Extract Data Field for reading zip file.
-
-read.zip <- function(files){
-        zipdir <-tempfile()
-        dir.create(zipdir)
-        unzip(files, exdir=zipdir)
-        filez<-list.files(zipdir)
-        do.call("rbind",lapply(paste(zipdir,filez,sep="/"), function (files){
-                                                    read.csv(files, header=T)
-                                                                }
-                               )
-                );
-
-}
-#####End of Read Zip function#####
 
 #####Clean Data Function#####
+
 cleanData <- function(dataset){
+    dataset <- head(dataset,100);
     datasetC<- dataset[!is.na(dataset$EndStation.Id),];
+    
     datasetC$Start.Date <- strptime(x=as.character(datasetC$Start.Date), format = "%d/%m/%Y %H:%M");
     datasetC$End.Date <- strptime(x=as.character(datasetC$End.Date), format = "%d/%m/%Y %H:%M");
-    datasetS <<- datasetC[order(datasetC$Bike.Id,datasetC$Start.Date),]; # sorts the data by BikeId,StartDate to aid in matching Start and End destinations
-    dBorCord <<- read.csv("..Data/Docking station Boroughs and Coords.csv", header=T); # load Borough and Coordinate data to be added to the final dataset
     
+    dBorCordStrt <<- read.csv("../Data/Docking station Boroughs and Coords.csv", colClasses= c(NA,NA,"NULL","NULL",NA,NA)
+                                  ,header=T, col.names=c('Start.Site','Start.Borough','Start.Easting','Start.Northing','Start.Latitude','Start.Longitude')); # load Borough and Coordinate data to be added to the final dataset
+    dBorCordEnd <<- read.csv("../Data/Docking station Boroughs and Coords.csv", colClasses= c(NA,NA,"NULL","NULL",NA,NA)
+                                  ,header=T, col.names=c('End.Site','End.Borough','End.Easting','End.Northing','End.Latitude','End.Longitude')); # load Borough and Coordinate data to be added to the final dataset
     
+    Start.Site <- sapply(datasetC$StartStation.Name, FUN=function(vec){trim(substring(vec, 1, regexpr(',',vec)[1]-1))});
+    End.Site <- sapply(datasetC$EndStation.Name, FUN=function(vec){trim(substring(vec, 1, regexpr(',',vec)[1]-1))});
+    
+    datasetC <- cbind(datasetC,Start.Site);
+    datasetC <- cbind(datasetC,End.Site);
+    
+    datasetBC <- merge(datasetC,dBorCordStrt, by.x="Start.Site",by.y="Start.Site", all.x=TRUE);
+    datasetBC <- merge(datasetBC,dBorCordEnd, by.x = "End.Site",by.y="End.Site", all.x=TRUE);
+    datasetS <<- datasetBC[order(datasetBC$Bike.Id,datasetBC$Start.Date),]; # sorts the data by BikeId,StartDate to aid in matching Start and End destinations
     
 }
 #####End of Clean Data Function#####
+
+#####Read from a Zip File#####
+# This is a sister function that is called in the Extract Data Function for reading zip file.
+
+read.zip <- function(files){
+  zipdir <-tempfile()
+  dir.create(zipdir)
+  unzip(files, exdir=zipdir)
+  filez<-list.files(zipdir)
+  do.call("rbind",lapply(paste(zipdir,filez,sep="/"), function (files){
+    read.csv(files, header=T)
+                             }
+                        )
+    );
+  
+}
+#####End of Read Zip function#####
+
+#####Trim Function#####
+
+trim <- function(vec) {
+  gsub("^\\s+|\\s+$", "", vec);
+}
+#####End of Trim Function####
